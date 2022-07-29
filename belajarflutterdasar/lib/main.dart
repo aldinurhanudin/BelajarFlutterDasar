3fe520e3f691396abc7c836c7add2c9c5c7e6402
Aldi Nurhanudin <aldinurhanudin08@gmail.com>
2022-07-29 22:38:46 +0700

    tutorial 8images/assets

--- belajarflutterdasar/lib/main.dart
+++ belajarflutterdasar/lib/main.dart
@@ -9,35 +9,34 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('my first App'),
-          centerTitle: true,
-          // colours
-          backgroundColor: Colors.red[600],
-          // akhir
-        ),
-        body: Center(
-          child: Text(
-            'Hello Zayn malik',
-            // fonts
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              backgroundColor: Colors.grey[600],
-              fontFamily: 'IndieFlower',
-            ),
-            // akhir
+      appBar: AppBar(
+        title: Text('my first App'),
+        centerTitle: true,
+        // colours
+        backgroundColor: Colors.red[600],
+        // akhir
+      ),
+      body: Center(
+          // images
+          //untuk manggil image di assets
+          // child: Image(
+          //   image: AssetImage('assets/beauty.jpg'),
+          //akhir untuk manggil image di assets
+          // Manggil dari network
+          child: Image.network(
+              'https://images.unsplash.com/photo-1657214663963-5030959d77d7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80')
+          // akhir dari network
+          // ),
+          // akhir images
           ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Text('click me'),
-          // colours
-          backgroundColor: Colors.red[600],
-          // akhir
-        ),
-      );
-      // Akhir statlesswidget
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('click me'),
+        // colours
+        backgroundColor: Colors.red[600],
+        // akhir
+      ),
+    );
+    // Akhir statlesswidget
   }
 }
