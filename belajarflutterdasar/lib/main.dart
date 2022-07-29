9655e0bca728069d3f3082076dd321550e7469e8
Aldi Nurhanudin <aldinurhanudin08@gmail.com>
2022-07-30 00:02:14 +0700

    13flutteroutline/shorcuts

--- belajarflutterdasar/lib/main.dart
+++ belajarflutterdasar/lib/main.dart
@@ -22,10 +22,6 @@ class Home extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
-              padding: EdgeInsets.all(20.0),
-              color: Colors.cyan,
-              child: Text('one')),
-          Container(
               padding: EdgeInsets.all(30.0),
               color: Colors.pinkAccent,
               child: Text('Two')),
@@ -33,6 +29,10 @@ class Home extends StatelessWidget {
               padding: EdgeInsets.all(40.0),
               color: Colors.lightGreen,
               child: Text('Three')),
+          Container(
+              padding: EdgeInsets.all(20.0),
+              color: Colors.cyan,
+              child: Text('one')),
         ],
       ),
       // akhir columns
