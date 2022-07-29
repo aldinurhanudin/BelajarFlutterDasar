3eeaf1e4ea332a7417c3429c030452a22e856a11
Aldi Nurhanudin <aldinurhanudin08@gmail.com>
2022-07-29 17:14:42 +0700

    6colours/fonts

--- belajarflutterdasar/lib/main.dart
+++ belajarflutterdasar/lib/main.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('my first App'), 
-      centerTitle: true,
-    ),
-    body: Center(
-      child: Text('Hello Zayn malik'),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {  },
-      child: Text('click'),
-    ),
-  ),
-));
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('my first App'),
+          centerTitle: true,
+          // colours
+          backgroundColor: Colors.red[600],
+          // akhir
+        ),
+        body: Center(
+          child: Text(
+            'Hello Zayn malik',
+            // fonts
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              backgroundColor: Colors.grey[600],
+              fontFamily: 'IndieFlower',
+            ),
+            // akhir
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Text('click'),
+          // colours
+          backgroundColor: Colors.red[600],
+          // akhir
+        ),
+      ),
+    ));
