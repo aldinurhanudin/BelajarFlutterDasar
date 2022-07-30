3e33f83a8b15223c883c81424ade4e6ae6458089
Aldi Nurhanudin <aldinurhanudin08@gmail.com>
2022-07-30 20:47:39 +0700

    19cards

--- belajarflutterdasar/lib/main.dart
+++ belajarflutterdasar/lib/main.dart
@@ -1,36 +1,54 @@
 import 'package:flutter/material.dart';
 import 'quote.dart';
 
-void main() => runApp(MaterialApp(
-      home: QuoteList(),
-    ));
+void main() => runApp(MaterialApp(home: QuoteList()));
 
 class QuoteList extends StatefulWidget {
   @override
-  State<QuoteList> createState() => _QuoteListState();
+  _QuoteListState createState() => _QuoteListState();
 }
 
 class _QuoteListState extends State<QuoteList> {
   List<Quote> quotes = [
     Quote(
-      author: 'Osca Wilde',
-      text: 'Be yourself, everyone else is already taken',
-      'I have nothing to declare except my genius',
-      'The truth is rarely pure and necer simple',
-    ),
+        author: 'Oscar Wilde',
+        text: 'Be yourself; everyone else is already taken'),
     Quote(
-      author: 'Osca Wilde',
-      text: 'Be yourself, everyone else is already taken',
-      'I have nothing to declare except my genius',
-      'The truth is rarely pure and necer simple',
-    ),
+        author: 'Oscar Wilde',
+        text: 'I have nothing to declare except my genius'),
     Quote(
-      author: 'Osca Wilde',
-      text: 'Be yourself, everyone else is already taken',
-      'I have nothing to declare except my genius',
-      'The truth is rarely pure and necer simple',
-    ),
+        author: 'Oscar Wilde',
+        text: 'The truth is rarely pure and never simple')
   ];
+
+  Widget quoteTemplate(quote) {
+    return Card(
+        margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Text(
+                quote.text,
+                style: TextStyle(
+                  fontSize: 18.0,
+                  color: Colors.grey[600],
+                ),
+              ),
+              SizedBox(height: 6.0),
+              Text(
+                quote.author,
+                style: TextStyle(
+                  fontSize: 14.0,
+                  color: Colors.grey[800],
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,19 +59,8 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes
-            .map(
-              (quote) => Text('${quote.text} - ${quote.author}'),
-            )
-            .toList(),
+        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
       ),
     );
   }
-}
-
-class Qoute {
-  String? text;
-  String? author;
-
-  Qoute({required String this.text, required String this.author});
 }
