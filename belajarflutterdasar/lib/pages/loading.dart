1753db4d4d323eee1cdf6a0ee38454e137a3aed0
Aldi Nurhanudin <aldinurhanudin08@gmail.com>
2022-07-31 00:26:29 +0700

    27worldtimeapi

--- belajarflutterdasar/lib/pages/loading.dart
+++ belajarflutterdasar/lib/pages/loading.dart
@@ -1,24 +1,34 @@
 part of 'pages.dart';
 
 class Loading extends StatefulWidget {
-  const Loading({Key? key}) : super(key: key);
-
   @override
-  State<Loading> createState() => _LoadingState();
+  _LoadingState createState() => _LoadingState();
 }
 
 class _LoadingState extends State<Loading> {
-  void getData() async {
-   Response response = await get('https://jsonplaceholder.typicode.com/todos/1');
-    // print(response.body);
-   Map data =jsonDecode(response.body);
-   print(data);
-   print(data['title']);
+
+  void getTime() async {
+    // make the request
+    Response response = await get('http://worldtimeapi.org/api/timezone/Europe/London');
+    Map data = jsonDecode(response.body);
+    //print(data);
+
+    // get properties from json
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1,3);
+    //print(datetime);
+    //print(offset);
+
+    // create DateTime object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
   }
-    
+
   @override
   void initState() {
     super.initState();
+    getTime();
   }
 
   @override
